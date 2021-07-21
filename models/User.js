e4f13e4ebad72ec8174d9c3cf8e79df54457b27d
Samantha Malone <sammac99@gmail.com>
2021-07-20 18:03:04 -0700

    added query call option

--- models/User.js
+++ models/User.js
@@ -41,6 +41,11 @@ User.init(
     newUserData.password = await bcrypt.hash(newUserData.password, 10);
       return newUserData;
     },
+    // set up beforeUpdate lifecycle "hook" functionality
+  async beforeUpdate(updatedUserData) {
+    updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+    return updatedUserData;
+  }
   },
     sequelize,
     timestamps: false,
