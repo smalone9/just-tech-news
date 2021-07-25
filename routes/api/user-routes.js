129d1be195bf253ec5b80397acbfcf633cbec16f
Samantha Malone <sammac99@gmail.com>
2021-07-24 21:58:13 -0700

    updated get rutes to include vote data

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
     User.findAll({
-      attributes: { exclude: ['password'] }
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
     })
       .then(dbUserData => res.json(dbUserData))
       .catch(err => {
@@ -17,10 +23,18 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
-      attributes: { exclude: ['password'] },
-      where: {
-        id: req.params.id
-      }
+      include: [
+        {
+          model: Post,
+          attributes: ['id', 'title', 'post_url', 'created_at']
+        },
+        {
+          model: Post,
+          attributes: ['title'],
+          through: Vote,
+          as: 'voted_posts'
+        }
+      ]
     })
       .then(dbUserData => {
         if (!dbUserData) {
