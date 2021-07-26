c7775f598f32415a3d9c52487dab421e0398d73f
Samantha Malone <sammac99@gmail.com>
2021-07-26 15:23:12 -0700

    set up initial view scaffold and handlebars

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const path = require('path');;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// set up Handlebars.js as template engine
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set up Handlebars.js as template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
