6261cdc94011f3a687d253bf124694b65952d725
Samantha Malone <sammac99@gmail.com>
2021-07-26 16:53:58 -0700

    added start session

--- server.js
+++ server.js
@@ -9,6 +9,22 @@ const sequelize = require('./config/connection');
 // set up Handlebars.js as template engine
 const hbs = exphbs.create({});
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 // set up Handlebars.js as template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
