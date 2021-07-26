3b7283008fea9ed62f7a9a8073b77034f15b37b8
Samantha Malone <sammac99@gmail.com>
2021-07-26 15:57:18 -0700

    refactored code

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const path = require('path');;
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
 // set up Handlebars.js as template engine
-const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // set up Handlebars.js as template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // turn on routes
-app.use(routes);
+app.use(require('./controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
